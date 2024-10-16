104e5d2a6f99b5b6b6ea36d13696a526ba74e597
Pan Mateusz <57064828+Laavenx@users.noreply.github.com>
2024-10-16 11:24:48 +0200

    added otherprojects

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,12 @@
 function DisplayContextSwitcher(context) {
-  if (context == "Projects") {
-    fetch("pages/projects.html")
-    .then((response) => response.text())
-    .then((html) => {
-        document.getElementById("content-container").innerHTML = html;
-    })
-    .catch((error) => {
-        console.warn(error);
-    });
-  } else {
-    fetch("pages/skills.html")
-    .then((response) => response.text())
-    .then((html) => {
-        document.getElementById("content-container").innerHTML = html;
-    })
-    .catch((error) => {
-        console.warn(error);
-    });
-  }
+  fetch("pages/" + context +".html")
+  .then((response) => response.text())
+  .then((html) => {
+      document.getElementById("content-container").innerHTML = html;
+  })
+  .catch((error) => {
+      console.warn(error);
+  });
 }
 
 function ProjectDisplayContextSwitcher(context) {
